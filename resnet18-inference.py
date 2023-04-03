7605a9427cc8dc30a780300eb56021b7a6e937e6
fivefishstudios <diy@fivefishstudios.com>
2023-04-03 00:03:16 -0500

    train and inference using resnet18

---
+++ resnet18-inference.py
@@ -0,0 +1,228 @@
+# Inferencing: Load saved model and inference a sample photo
+# Bird Classification
+# 3/31/23 
+# by @owel.codes
+
+import os
+import requests
+import zipfile
+from pathlib import Path
+import torch
+import torchvision
+from torch import nn
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+from tqdm.auto import tqdm
+from timeit import default_timer as timer
+from torchinfo import summary
+from PIL import Image
+import matplotlib.pyplot as plt
+import cv2
+from joblib.externals.loky.backend.context import get_context
+
+# model location
+MODEL_PATH = Path("models")
+MODEL_NAME = "bird-resnet18-510species.pth"
+MODEL_PATH_NAME = MODEL_PATH / MODEL_NAME
+
+# NOTE: 510 Bird Species
+class_names = ['ABBOTTS BABBLER', 'ABBOTTS BOOBY', 'ABYSSINIAN GROUND HORNBILL', 'AFRICAN CROWNED CRANE', 
+               'AFRICAN EMERALD CUCKOO', 'AFRICAN FIREFINCH', 'AFRICAN OYSTER CATCHER', 'AFRICAN PIED HORNBILL', 
+               'AFRICAN PYGMY GOOSE', 'ALBATROSS', 'ALBERTS TOWHEE', 'ALEXANDRINE PARAKEET', 'ALPINE CHOUGH', 
+               'ALTAMIRA YELLOWTHROAT', 'AMERICAN AVOCET', 'AMERICAN BITTERN', 'AMERICAN COOT', 'AMERICAN FLAMINGO', 
+               'AMERICAN GOLDFINCH', 'AMERICAN KESTREL', 'AMERICAN PIPIT', 'AMERICAN REDSTART', 'AMERICAN ROBIN', 
+               'AMERICAN WIGEON', 'AMETHYST WOODSTAR', 'ANDEAN GOOSE', 'ANDEAN LAPWING', 'ANDEAN SISKIN', 
+               'ANHINGA', 'ANIANIAU', 'ANNAS HUMMINGBIRD', 'ANTBIRD', 'ANTILLEAN EUPHONIA', 'APAPANE', 
+               'APOSTLEBIRD', 'ARARIPE MANAKIN', 'ASHY STORM PETREL', 'ASHY THRUSHBIRD', 'ASIAN CRESTED IBIS', 
+               'ASIAN DOLLARD BIRD', 'ASIAN GREEN BEE EATER', 'AUCKLAND SHAQ', 'AUSTRAL CANASTERO', 'AUSTRALASIAN FIGBIRD', 
+               'AVADAVAT', 'AZARAS SPINETAIL', 'AZURE BREASTED PITTA', 'AZURE JAY', 'AZURE TANAGER', 'AZURE TIT', 
+               'BAIKAL TEAL', 'BALD EAGLE', 'BALD IBIS', 'BALI STARLING', 'BALTIMORE ORIOLE', 'BANANAQUIT', 'BAND TAILED GUAN', 'BANDED BROADBILL', 'BANDED PITA', 'BANDED STILT', 'BAR-TAILED GODWIT', 'BARN OWL', 'BARN SWALLOW', 'BARRED PUFFBIRD', 'BARROWS GOLDENEYE', 'BAY-BREASTED WARBLER', 'BEARDED BARBET', 'BEARDED BELLBIRD', 'BEARDED REEDLING', 'BELTED KINGFISHER', 'BIRD OF PARADISE', 'BLACK AND YELLOW BROADBILL', 'BLACK BAZA', 'BLACK COCKATO', 'BLACK FACED SPOONBILL', 'BLACK FRANCOLIN', 'BLACK HEADED CAIQUE', 'BLACK NECKED STILT', 'BLACK SKIMMER', 'BLACK SWAN', 'BLACK TAIL CRAKE', 'BLACK THROATED BUSHTIT', 'BLACK THROATED HUET', 'BLACK THROATED WARBLER', 'BLACK VENTED SHEARWATER', 'BLACK VULTURE', 'BLACK-CAPPED CHICKADEE', 'BLACK-NECKED GREBE', 'BLACK-THROATED SPARROW', 'BLACKBURNIAM WARBLER', 'BLONDE CRESTED WOODPECKER', 'BLOOD PHEASANT', 'BLUE COAU', 'BLUE DACNIS', 'BLUE GRAY GNATCATCHER', 'BLUE GROSBEAK', 'BLUE GROUSE', 'BLUE HERON', 'BLUE MALKOHA', 'BLUE THROATED TOUCANET', 'BOBOLINK', 'BORNEAN BRISTLEHEAD', 'BORNEAN LEAFBIRD', 'BORNEAN PHEASANT', 'BRANDT CORMARANT', 'BREWERS BLACKBIRD', 'BROWN CREPPER', 'BROWN HEADED COWBIRD', 'BROWN NOODY', 'BROWN THRASHER', 'BUFFLEHEAD', 'BULWERS PHEASANT', 'BURCHELLS COURSER', 'BUSH TURKEY', 'CAATINGA CACHOLOTE', 'CACTUS WREN', 'CALIFORNIA CONDOR', 'CALIFORNIA GULL', 'CALIFORNIA QUAIL', 'CAMPO FLICKER', 'CANARY', 'CANVASBACK', 'CAPE GLOSSY STARLING', 'CAPE LONGCLAW', 'CAPE MAY WARBLER', 'CAPE ROCK THRUSH', 'CAPPED HERON', 'CAPUCHINBIRD', 'CARMINE BEE-EATER', 'CASPIAN TERN', 'CASSOWARY', 'CEDAR WAXWING', 'CERULEAN WARBLER', 'CHARA DE COLLAR', 'CHATTERING LORY', 'CHESTNET BELLIED EUPHONIA', 'CHINESE BAMBOO PARTRIDGE', 'CHINESE POND HERON', 'CHIPPING SPARROW', 'CHUCAO TAPACULO', 'CHUKAR PARTRIDGE', 'CINNAMON ATTILA', 'CINNAMON FLYCATCHER', 'CINNAMON TEAL', 'CLARKS GREBE', 'CLARKS NUTCRACKER', 'COCK OF THE  ROCK', 'COCKATOO', 'COLLARED ARACARI', 'COLLARED CRESCENTCHEST', 'COMMON FIRECREST', 'COMMON GRACKLE', 'COMMON HOUSE MARTIN', 'COMMON IORA', 'COMMON LOON', 'COMMON POORWILL', 'COMMON STARLING', 'COPPERSMITH BARBET', 'COPPERY TAILED COUCAL', 'CRAB PLOVER', 'CRANE HAWK', 'CREAM COLORED WOODPECKER', 'CRESTED AUKLET', 'CRESTED CARACARA', 'CRESTED COUA', 'CRESTED FIREBACK', 'CRESTED KINGFISHER', 'CRESTED NUTHATCH', 'CRESTED OROPENDOLA', 'CRESTED SERPENT EAGLE', 'CRESTED SHRIKETIT', 'CRESTED WOOD PARTRIDGE', 'CRIMSON CHAT', 'CRIMSON SUNBIRD', 'CROW', 'CROWNED PIGEON', 'CUBAN TODY', 'CUBAN TROGON', 'CURL CRESTED ARACURI', 'D-ARNAUDS BARBET', 'DALMATIAN PELICAN', 'DARJEELING WOODPECKER', 'DARK EYED JUNCO', 'DAURIAN REDSTART', 'DEMOISELLE CRANE', 'DOUBLE BARRED FINCH', 'DOUBLE BRESTED CORMARANT', 'DOUBLE EYED FIG PARROT', 'DOWNY WOODPECKER', 'DUSKY LORY', 'DUSKY ROBIN', 'EARED PITA', 'EASTERN BLUEBIRD', 'EASTERN BLUEBONNET', 'EASTERN GOLDEN WEAVER', 'EASTERN MEADOWLARK', 'EASTERN ROSELLA', 'EASTERN TOWEE', 'EASTERN WIP POOR WILL', 'EASTERN YELLOW ROBIN', 'ECUADORIAN HILLSTAR', 'EGYPTIAN GOOSE', 'ELEGANT TROGON', 'ELLIOTS  PHEASANT', 'EMERALD TANAGER', 'EMPEROR PENGUIN', 'EMU', 'ENGGANO MYNA', 'EURASIAN BULLFINCH', 'EURASIAN GOLDEN ORIOLE', 'EURASIAN MAGPIE', 'EUROPEAN GOLDFINCH', 'EUROPEAN TURTLE DOVE', 'EVENING GROSBEAK', 'FAIRY BLUEBIRD', 'FAIRY PENGUIN', 'FAIRY TERN', 'FAN TAILED WIDOW', 'FASCIATED WREN', 'FIERY MINIVET', 'FIORDLAND PENGUIN', 'FIRE TAILLED MYZORNIS', 'FLAME BOWERBIRD', 'FLAME TANAGER', 'FOREST WAGTAIL', 'FRIGATE', 'FRILL BACK PIGEON', 'GAMBELS QUAIL', 'GANG GANG COCKATOO', 'GILA WOODPECKER', 'GILDED FLICKER', 'GLOSSY IBIS', 'GO AWAY BIRD', 'GOLD WING WARBLER', 'GOLDEN BOWER BIRD', 'GOLDEN CHEEKED WARBLER', 'GOLDEN CHLOROPHONIA', 'GOLDEN EAGLE', 'GOLDEN PARAKEET', 'GOLDEN PHEASANT', 'GOLDEN PIPIT', 'GOULDIAN FINCH', 'GRANDALA', 'GRAY CATBIRD', 'GRAY KINGBIRD', 'GRAY PARTRIDGE', 'GREAT ARGUS', 'GREAT GRAY OWL', 'GREAT JACAMAR', 'GREAT KISKADEE', 'GREAT POTOO', 'GREAT TINAMOU', 'GREAT XENOPS', 'GREATER PEWEE', 'GREATER PRAIRIE CHICKEN', 'GREATOR SAGE GROUSE', 'GREEN BROADBILL', 'GREEN JAY', 'GREEN MAGPIE', 'GREEN WINGED DOVE', 'GREY CUCKOOSHRIKE', 'GREY HEADED FISH EAGLE', 'GREY PLOVER', 'GROVED BILLED ANI', 'GUINEA TURACO', 'GUINEAFOWL', 'GURNEYS PITTA', 'GYRFALCON', 'HAMERKOP', 'HARLEQUIN DUCK', 'HARLEQUIN QUAIL', 'HARPY EAGLE', 'HAWAIIAN GOOSE', 'HAWFINCH', 'HELMET VANGA', 'HEPATIC TANAGER', 'HIMALAYAN BLUETAIL', 'HIMALAYAN MONAL', 'HOATZIN', 'HOODED MERGANSER', 'HOOPOES', 'HORNED GUAN', 'HORNED LARK', 'HORNED SUNGEM', 'HOUSE FINCH', 'HOUSE SPARROW', 'HYACINTH MACAW', 'IBERIAN MAGPIE', 'IBISBILL', 'IMPERIAL SHAQ', 'INCA TERN', 'INDIAN BUSTARD', 'INDIAN PITTA', 'INDIAN ROLLER', 'INDIAN VULTURE', 'INDIGO BUNTING', 'INDIGO FLYCATCHER', 'INLAND DOTTEREL', 'IVORY BILLED ARACARI', 'IVORY GULL', 'IWI', 'JABIRU', 'JACK SNIPE', 'JACOBIN PIGEON', 'JANDAYA PARAKEET', 'JAPANESE ROBIN', 'JAVA SPARROW', 'JOCOTOCO ANTPITTA', 'KAGU', 'KAKAPO', 'KILLDEAR', 'KING EIDER', 'KING VULTURE', 'KIWI', 'KOOKABURRA', 'LARK BUNTING', 'LAUGHING GULL', 'LAZULI BUNTING', 'LESSER ADJUTANT', 'LILAC ROLLER', 'LIMPKIN', 'LITTLE AUK', 'LOGGERHEAD SHRIKE', 'LONG-EARED OWL', 'LOONEY BIRDS', 'LUCIFER HUMMINGBIRD', 'MAGPIE GOOSE', 'MALABAR HORNBILL', 'MALACHITE KINGFISHER', 'MALAGASY WHITE EYE', 'MALEO', 'MALLARD DUCK', 'MANDRIN DUCK', 'MANGROVE CUCKOO', 'MARABOU STORK', 'MASKED BOBWHITE', 'MASKED BOOBY', 'MASKED LAPWING', 'MCKAYS BUNTING', 'MERLIN', 'MIKADO  PHEASANT', 'MILITARY MACAW', 'MOURNING DOVE', 'MYNA', 'NICOBAR PIGEON', 'NOISY FRIARBIRD', 'NORTHERN BEARDLESS TYRANNULET', 'NORTHERN CARDINAL', 'NORTHERN FLICKER', 'NORTHERN FULMAR', 'NORTHERN GANNET', 'NORTHERN GOSHAWK', 'NORTHERN JACANA', 'NORTHERN MOCKINGBIRD', 'NORTHERN PARULA', 'NORTHERN RED BISHOP', 'NORTHERN SHOVELER', 'OCELLATED TURKEY', 'OKINAWA RAIL', 'ORANGE BRESTED BUNTING', 'ORIENTAL BAY OWL', 'ORNATE HAWK EAGLE', 'OSPREY', 'OSTRICH', 'OVENBIRD', 'OYSTER CATCHER', 'PAINTED BUNTING', 'PALILA', 'PALM NUT VULTURE', 'PARADISE TANAGER', 'PARAKETT  AKULET', 'PARUS MAJOR', 'PATAGONIAN SIERRA FINCH', 'PEACOCK', 'PEREGRINE FALCON', 'PHAINOPEPLA', 'PHILIPPINE EAGLE', 'PINK ROBIN', 'PLUSH CRESTED JAY', 'POMARINE JAEGER', 'PUFFIN', 'PUNA TEAL', 'PURPLE FINCH', 'PURPLE GALLINULE', 'PURPLE MARTIN', 'PURPLE SWAMPHEN', 'PYGMY KINGFISHER', 'PYRRHULOXIA', 'QUETZAL', 'RAINBOW LORIKEET', 'RAZORBILL', 'RED BEARDED BEE EATER', 'RED BELLIED PITTA', 'RED BILLED TROPICBIRD', 'RED BROWED FINCH', 'RED FACED CORMORANT', 'RED FACED WARBLER', 'RED FODY', 'RED HEADED DUCK', 'RED HEADED WOODPECKER', 'RED KNOT', 'RED LEGGED HONEYCREEPER', 'RED NAPED TROGON', 'RED SHOULDERED HAWK', 'RED TAILED HAWK', 'RED TAILED THRUSH', 'RED WINGED BLACKBIRD', 'RED WISKERED BULBUL', 'REGENT BOWERBIRD', 'RING-NECKED PHEASANT', 'ROADRUNNER', 'ROCK DOVE', 'ROSE BREASTED COCKATOO', 'ROSE BREASTED GROSBEAK', 'ROSEATE SPOONBILL', 'ROSY FACED LOVEBIRD', 
+               # ....
+               'ROUGH LEG BUZZARD', 'ROYAL FLYCATCHER', 'RUBY CROWNED KINGLET', 'RUBY THROATED HUMMINGBIRD', 
+               'RUDDY SHELDUCK', 'RUDY KINGFISHER', 'RUFOUS KINGFISHER', 'RUFOUS TREPE', 'RUFUOS MOTMOT', 
+               'SAMATRAN THRUSH', 'SAND MARTIN', 'SANDHILL CRANE', 'SATYR TRAGOPAN', 'SAYS PHOEBE', 
+               'SCARLET CROWNED FRUIT DOVE', 'SCARLET FACED LIOCICHLA', 'SCARLET IBIS', 'SCARLET MACAW', 
+               'SCARLET TANAGER', 'SHOEBILL', 'SHORT BILLED DOWITCHER', 'SMITHS LONGSPUR', 'SNOW GOOSE', 
+               'SNOWY EGRET', 'SNOWY OWL', 'SNOWY PLOVER', 'SORA', 'SPANGLED COTINGA', 'SPLENDID WREN', 
+               'SPOON BILED SANDPIPER', 'SPOTTED CATBIRD', 'SPOTTED WHISTLING DUCK', 'SQUACCO HERON', 
+               'SRI LANKA BLUE MAGPIE', 'STEAMER DUCK', 'STORK BILLED KINGFISHER', 'STRIATED CARACARA', 
+               'STRIPED OWL', 'STRIPPED MANAKIN', 'STRIPPED SWALLOW', 'SUNBITTERN', 'SUPERB STARLING', 'SURF SCOTER', 'SWINHOES PHEASANT', 'TAILORBIRD', 'TAIWAN MAGPIE', 'TAKAHE', 'TASMANIAN HEN', 'TAWNY FROGMOUTH', 'TEAL DUCK', 'TIT MOUSE', 'TOUCHAN', 'TOWNSENDS WARBLER', 'TREE SWALLOW', 'TRICOLORED BLACKBIRD', 'TROPICAL KINGBIRD', 'TRUMPTER SWAN', 'TURKEY VULTURE', 'TURQU-14.4184OISE MOTMOT', 'UMBRELLA BIRD', 'VARIED THRUSH', 'VEERY', 'VENEZUELIAN TROUPIAL', 'VERDIN', 'VERMILION FLYCATHER', 'VICTORIA CROWNED PIGEON', 'VIOLET BACKED STARLING', 'VIOLET GREEN SWALLOW', 'VIOLET TURACO', 'VISAYAN HORNBILL', 'VULTURINE GUINEAFOWL', 'WALL CREAPER', 'WATTLED CURASSOW', 'WATTLED LAPWING', 'WHIMBREL', 'WHITE BREASTED WATERHEN', 'WHITE BROWED CRAKE', 'WHITE CHEEKED TURACO', 'WHITE CRESTED HORNBILL', 'WHITE EARED HUMMINGBIRD', 'WHITE NECKED RAVEN', 'WHITE TAILED TROPIC', 'WHITE THROATED BEE EATER', 'WILD TURKEY', 'WILLOW PTARMIGAN', 
+               # ...
+               'WILSONS BIRD OF PARADISE', 'WOOD DUCK', 'WOOD THRUSH', 'WOODLAND KINGFISHER', 'WRENTIT', 
+               'YELLOW BELLIED FLOWERPECKER', 'YELLOW CACIQUE', 'YELLOW HEADED BLACKBIRD', 'ZEBRA DOVE']
+
+# load a sample unknown bird image
+filepathname = "./unknown/baltimore-oriole.jpeg"  
+# filepathname = "./unknown/barn-swallow1.jpeg"  # CORRECT
+# filepathname = "./unknown/bush-turkey1.jpeg"    # CORRECT
+# filepathname = "./unknown/canary1.jpg"   # PLAUSIBLE
+# filepathname = "./unknown/chipping-sparrow1.jpg"
+# filepathname = "./unknown/Northern-Cardinal.jpg"
+# filepathname = "./data/birds/test/BALTIMORE ORIOLE/5.jpg"
+# filepathname = "./data/birds/train/CANARY/004.jpg"
+# filepathname = "./unknown/bluebird-480px.jpg"
+# filepathname = "./unknown/owl2.jpg"
+# filepathname = "./unknown/turkey2.jpg"
+# filepathname = "./unknown/phil-eagle1.jpg"
+# filepathname = "./unknown/Philippine_Eagle2.jpg"
+# filepathname = "./unknown/pink-robin1.jpeg"
+# filepathname = "./unknown/mourning-dove1.jpg"
+# filepathname = "./unknown/Mourning-Dove-2.jpg"
+# filepathname = "./unknown/mourningdove3.jpg"
+# filepathname = "./unknown/cardinal1.jpg"
+# filepathname = "./unknown/Cardinal_2.jpg"
+# filepathname = "./unknown/northern-cardinal3.jpg"
+# filepathname = "./unknown/violet-turaco1.jpg"
+# filepathname = "./unknown/violet-turaco2.jpg"
+# filepathname = "./unknown/violet-turaco3.jpg"
+# filepathname = "./unknown/macan1.jpg"
+# filepathname = "./unknown/macan2.jpg"
+# filepathname = "./unknown/kingfisher1.jpeg"
+# filepathname = "./unknown/kingfisher2.jpg"
+# filepathname = "./unknown/puffin1.jpg"
+# filepathname = "./unknown/puffin2.jpg"
+# filepathname = "./unknown/puffin3.jpg"
+# filepathname = "./unknown/tasmanian-hen1.jpeg"
+# filepathname = "./unknown/tasmanian-hen2.jpg"
+# filepathname = "./unknown/paradise1.jpg"
+# filepathname = "./unknown/paradise2.jpg"
+# filepathname = "./unknown/titmouse1.jpg"
+# filepathname = "./unknown/tit-mouse2.jpg"
+# filepathname = "./unknown/turkey2.jpg"
+# filepathname = "./unknown/Mourning-Dove-2.jpg"
+# filepathname = "../BirdBrain-AI/data/test/AFRICAN PYGMY GOOSE/4.jpg"
+# filepathname = "../BirdBrain-AI/data/valid/AMERICAN GOLDFINCH/3.jpg"
+# filepathname = "../BirdBrain-AI/data/valid/AFRICAN OYSTER CATCHER/2.jpg"
+# filepathname = "../BirdBrain-AI/data/valid/AMERICAN PIPIT/1.jpg"
+# filepathname = "./unknown/bird-big-beak-1200.jpeg"
+# filepathname = "./unknown/red-head-bird.jpeg"
+# filepathname = "./unknown/bluebird-mountain.jpg"
+# filepathname = "./unknown/rainbow-bird.jpg"
+# filepathname = "./unknown/kingfisher-blue.jpg"
+# filepathname = "./unknown/brown-bird1.jpg"
+# filepathname = "./unknown/bluebird-branch.jpeg"
+
+
+# setup cuda if available
+device = "cuda" if torch.cuda.is_available() else "cpu"
+# print(f"NOTE: All calculations will be done in {device}")
+
+
+data_transform = transforms.Compose([  # Compose is used to serialize our tranforms functions
+    # transform operations are a list
+    # 1. resize image to be smaller
+    transforms.Resize(size=(224, 224), antialias=True),
+    # 2. do some data augmentation
+    transforms.RandomHorizontalFlip(p=0.25),  # flip horizontal 25% of the time
+    transforms.RandomRotation(degrees=30),  # test_acc = 0.6710
+    # transforms.RandomAffine(degrees=15),      # test_acc = 0.6347
+    # 3. convert to Tensor so PyTorch can use the data
+    transforms.ToTensor(),  # ultimate goal is to convert to Tensors
+    transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                         std=[0.229, 0.224, 0.225])
+])
+
+
+# load our base model to be re-used for transfer learning
+# TRANSFER LEARNING -> Setup the model with pretrained weights and send it to the target device (torchvision
+# v0.13+)
+# weights = torchvision.models.EfficientNet_B0_Weights.DEFAULT  # .DEFAULT = best available weights
+# model = torchvision.models.efficientnet_b0(weights=weights).to(device)
+model  = torchvision.models.resnet18().to(device)
+
+# Get the length of class_names (one output unit for each class)
+output_shape = len(class_names)
+
+# override classifier method in model class
+# Recreate the classifier layer and seed it to the target device
+model.classifier = torch.nn.Sequential(
+    torch.nn.Dropout(p=0.2, inplace=True),
+    torch.nn.Linear(in_features=1280,
+                    out_features=output_shape,  # same number of output units as our number of classes
+                    )).to(device)
+                    # bias=True)).to(device)
+
+
+# create test_step() function
+def eval_step(model: torch.nn.Module,
+              dataloader: torch.utils.data.DataLoader,
+              loss_fn: torch.nn.Module):
+    # put model in eval mode.
+    model.eval()
+
+    # setup loss and acc counters
+    test_loss, test_acc = 0, 0
+
+    # put in inference mode
+    with torch.inference_mode():
+        # loop through dataloader in batches
+        for batch, (X, y) in enumerate(dataloader):
+            # send values to target device
+            X, y = X.to(device), y.to(device)
+
+            # forward pass
+            test_pred_logits = model(X)
+
+            # calc and accumulate loss
+            loss = loss_fn(test_pred_logits, y)
+            test_loss += loss.item()
+
+            # calc and accumulate accuracy
+            test_pred_labels = test_pred_logits.argmax(dim=1)
+            test_acc += (test_pred_labels == y).sum().item() / len(test_pred_labels)
+
+    # get average of each batch
+    test_loss = test_loss / len(dataloader)
+    test_acc = test_acc / len(dataloader)
+    return test_loss, test_acc
+
+
+# instantiate model
+# model = TinyVGG(input_shape=3,  # number of color channels RGB=3
+#                 hidden_units=NODES,
+#                 output_shape=len(class_names)
+#                 ).to(device)
+
+# load model from disk
+model.load_state_dict(torch.load(f=MODEL_PATH_NAME))
+
+start_time = timer()
+unknown_img = cv2.imread(filepathname)
+# openCV uses BGR so we need to convert to RGB
+unknown_img_rgb = cv2.cvtColor(unknown_img, cv2.COLOR_BGR2RGB)
+# print(f"unknown image shape is: {unknown_img.shape}")   # height, width, channel
+
+# cv2.imshow('Preview', unknown_img)
+# cv2.waitKey()
+
+img_transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Resize(size=(224, 224), antialias=True),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                         std=[0.229, 0.224, 0.225])
+])
+
+# resize image to 224x224, same size expected by our model
+unknown_img_t = img_transform(unknown_img_rgb)  # convert to tensor
+unknown_img_t = unknown_img_t.unsqueeze(dim=0)  # add batch size
+
+# print(f"transformed image shape is: {unknown_img_t.shape}")
+unknown_img_t = unknown_img_t.to(device)
+
+model.eval()
+with torch.inference_mode():
+    predicted_class_logits = model(unknown_img_t)
+    print(f"\n\nfilepathname: {filepathname}")
+    # print(f"\nlogits: {predicted_class_logits}")
+    print(f"predicted object is a: {class_names[torch.argmax(predicted_class_logits)]}")
+    # shadow text
+    # cv2.putText(unknown_img, class_names[torch.argmax(predicted_class_logits)], (33, 53), cv2.FONT_HERSHEY_SIMPLEX, 1.12,
+    #             (20, 20, 20), 3)
+    # # foreground text
+    # cv2.putText(unknown_img, class_names[torch.argmax(predicted_class_logits)], (30, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.12,
+    #             (20, 255, 20), 3)
+    # cv2.imshow(class_names[torch.argmax(predicted_class_logits)], unknown_img)
+    end_time = timer()
+    print(f"total inference time is: {end_time - start_time} seconds\n\n")
+
+cv2.waitKey()
